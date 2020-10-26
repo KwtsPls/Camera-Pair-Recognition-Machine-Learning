10b90d6e58906af7d7a9f0942ea889ae95e0583e
KwtsPls <constantinepls99@gmail.com>
2020-10-26 23:23:30 +0200

    v.2

--- ErrorHandler.c
+++ ErrorHandler.c
@@ -5,6 +5,6 @@ int errorCode;
 
 //Function to print current error
 void print_error(){
-    if(errorCode==DIR_UNABLE_TO_OPEN) printf("Unable to read directory.\n");
-    else if(errorCode==DATA_X_NOT_FOUND) printf("Dataset X not found within program files... \nPlease insert the database in the project's folder.\n");
+    if(errorCode==DIR_UNABLE_TO_OPEN) perror("Unable to read directory.\n");
+    else if(errorCode==DATA_X_NOT_FOUND) perror("Dataset X not found within program files... \nPlease insert the database in the project's folder.\n");
 }
