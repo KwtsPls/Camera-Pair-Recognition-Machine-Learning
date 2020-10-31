0af4efe33d406b0892c3130bae9528d15b1ef0d3
= <lstetsikas3@gmail.com>
2020-10-31 21:24:11 +0200

    Dictionaries for Storing Json Values

--- Dictionary.c
+++ Dictionary.c
@@ -4,8 +4,10 @@
 #include "Dictionary.h"
 
 //Function to initialize a dictionary
-Dictionary initDictionary(){
+Dictionary *initDictionary(char *dict_name){
     Dictionary *dict = malloc(sizeof(Dictionary));
+    dict->dict_name = strdup(dict_name);
+    dict->list = NULL;
     return dict;
 }
 
@@ -20,4 +22,63 @@ Dictionary *insertDictionary(Dictionary *dic, char *key, char **val, int num_val
     node->next = dic->list;
     dic->list = node;
     return dic;
+}
+
+
+//Function for printing dictionary
+void printDictionary(Dictionary *dic)
+{
+    Dictionary_node *tmp;
+    printf("Kati den doulevei?\n");
+    tmp = dic->list;
+    while(tmp!=NULL)
+    {
+        printf("KEY:%s\n", tmp->key);
+        for(int i=0;i<tmp->values_num;i++)
+            printf("Value %d: %s\n",i,tmp->value[i]);
+        tmp = tmp->next;
+    }
+    
+}
+
+
+//Give Key in the Dictionary to get the values
+char **lookUpDictionary(Dictionary *dic, char *key, int *num_val)
+{
+    Dictionary_node *tmp;
+    tmp = dic->list;
+    while(tmp!=NULL)
+    {
+        if(strcmp(tmp->key,key)==0)
+        {
+            printf("TOVRIKA\n");
+            (*num_val) = tmp->values_num;
+            return tmp->value;
+        }
+        tmp = tmp->next;
+    }
+    return NULL;
+}
+
+//Function for Deleting Dictionaries
+void deleteDictionary(Dictionary **dic)
+{   
+    Dictionary_node *tmp, *next;
+    next = (*dic)->list;
+    while(next != NULL)
+    {
+        tmp = next;
+        next = next->next;
+        tmp -> next = NULL;
+        free(tmp->key);
+        for(int i=0;i<tmp->values_num;i++)
+            free(tmp->value[i]);
+        free(tmp->value);
+        free(tmp);
+        tmp = NULL;
+    }
+    (*dic)->list = NULL;
+    free((*dic)->dict_name);
+    free(*dic);
+    dic = NULL;
 }
--- Dictionary.h
+++ Dictionary.h
@@ -1,10 +1,7 @@
 #ifndef PROJECT_DICTIONARY_H
 #define PROJECT_DICTIONARY_H
 
-typedef struct dict{
-    char *dict_name;
-    Dictionary_node *list;
-}Dictionary;
+
 
 typedef struct dict_node{
     char *key;
@@ -13,10 +10,29 @@ typedef struct dict_node{
     struct dict_node *next;
 }Dictionary_node;
 
+
+typedef struct dict{
+    char *dict_name;
+    Dictionary_node *list;
+}Dictionary;
+
+
+
 //Function to initialize a dictionary
-Dictionary initDictionary();
+Dictionary *initDictionary(char *dict_name);
 
 //Function for inserting in dictionary
 Dictionary *insertDictionary(Dictionary *dic, char *key, char **val, int num_val);
+
+//Function for printing dictionary
+void printDictionary(Dictionary *dic);
+
+
+//Give Key in the Dictionary to get the values
+char **lookUpDictionary(Dictionary *dic, char *key, int *num_val);
+
+
+//Function for Deleting Dictionaries
+void deleteDictionary(Dictionary **dic);
 
 #endif //PROJECT_DICTIONARY_H
